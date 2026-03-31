e6466c180371b2af47754fd8c74260368b9e2e1b
ahmedhamzarana <ahmedhamzarana000@gmail.com>
2026-03-31 15:16:43 +0500

    initial commit

--- lib/providers/splash_provider.dart
+++ lib/providers/splash_provider.dart
@@ -5,12 +5,16 @@ import 'package:task_management_app/utils/app_routes.dart';
 class SplashProvider extends ChangeNotifier {
   final FlutterSecureStorage storage = FlutterSecureStorage();
   void splashTimer(BuildContext context) async {
-    await Future.delayed(const Duration(seconds: 3),);
-    if (context.mounted) {
-      Navigator.pushReplacementNamed(
-        context,
-        AppRoutes.loginRoute,
-      );
+    if (await storage.containsKey(key: "email") &&
+        await storage.containsKey(key: "password")) {
+      await Future.delayed(const Duration(seconds: 3));
+      if (context.mounted) {
+        Navigator.pushReplacementNamed(context, AppRoutes.appmainRoute);
+      }
+    } else {
+      if (context.mounted) {
+        Navigator.pushReplacementNamed(context, AppRoutes.loginRoute);
+      }
     }
   }
 }

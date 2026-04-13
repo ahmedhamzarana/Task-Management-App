f5b0755c594eafd307f7b5b741c10c96f7e930f1
ahmedhamzarana <ahmedhamzarana000@gmail.com>
2026-04-13 11:40:30 +0500

    Fix Problems

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -3,13 +3,11 @@ import 'package:flutter/material.dart';
 class AppColors {
   // Primary Colors
   static const Color bglight = Colors.white;
-  static const Color primary = Color.fromARGB(255, 224, 68, 6);
-  static const Color secondry = Colors.black;
+  static const Color primary = Color.fromARGB(255, 226, 70, 8);
+  static const Color secondry = Color.fromARGB(255, 0, 0, 0);
 
   // Status Colors
   static const Color info = Colors.grey;
   static const Color success = Colors.green;
   static const Color danger = Colors.red;
-
-  // Extended Colors - use const where possible
 }
